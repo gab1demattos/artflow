590b3a771cffc3bac98d74a47c21ad8953c263ff
gab1demattos <gab1demattos@yahoo.com>
2025-03-29 12:26:38 +0000

    creating database tables

---
+++ database.sql
@@ -0,0 +1,48 @@
+CREATE TABLE users {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_type TEXT NOT NULL CHECK (user_type IN ('freelancer', 'client')),
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT NOT NULL
+};
+
+CREATE TABLE services {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    category TEXT NOT NULL,
+    price REAL NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+};
+
+CREATE TABLE transactions {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    freelancer_id INTEGER NOT NULL,
+    client_id INTEGER NOT NULL,
+    service_id INTEGER NOT NULL,
+    status TEXT NOT NULL CHECK (status IN ('in progress', 'completed', 'cancelled')),
+    amount REAL NOT NULL,
+    FOREIGN KEY (freelancer_id) REFERENCES users(id),
+    FOREIGN KEY (client_id) REFERENCES users(id),
+    FOREIGN KEY (service_id) REFERENCES services(id)
+};
+
+CREATE TABLE messages {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    sender_id INTEGER NOT NULL,
+    receiver_id INTEGER NOT NULL,
+    message TEXT NOT NULL,
+    FOREIGN KEY (sender_id) REFERENCES users(id),
+    FOREIGN KEY (receiver_id) REFERENCES users(id)
+};
+
+CREATE TABLE reviews {
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    service_id INTEGER NOT NULL,
+    rating INTEGER NOT NULL CHECK (rating BETWEEN 1 AND 5),
+    comment TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (service_id) REFERENCES services(id)
+};
