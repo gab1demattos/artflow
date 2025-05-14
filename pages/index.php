9552e9a422c1ed03329b34dc4bb4079b1634c1e2
gab1demattos <gab1demattos@yahoo.com>
2025-05-14 12:13:54 +0100

    code organized

--- pages/index.php
+++ pages/index.php
@@ -1,6 +1,6 @@
 <?php 
-require_once(__DIR__ . '/../includes/session.php');
-require_once(__DIR__ . '/../includes/categories.php');
+require_once(__DIR__ . '/../database/session.php');
+require_once(__DIR__ . '/../database/categories.php');
 require_once(__DIR__ . '/../templates/home.tpl.php');
 
 $session = Session::getInstance();
