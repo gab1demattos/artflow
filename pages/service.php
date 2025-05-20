1dca15f719c818d478a11fec968709d730c45721
gab1demattos <gab1demattos@yahoo.com>
2025-05-20 21:30:54 +0100

    fixed something

--- pages/service.php
+++ pages/service.php
@@ -12,7 +12,7 @@ $db = Database::getInstance();
 
 $serviceId = isset($_GET['id']) ? intval($_GET['id']) : 0;
 
-$stmt = $db->prepare('SELECT Service.*, User.username FROM Service JOIN User ON Service.user_id = User.id WHERE Serviceid = ?');
+$stmt = $db->prepare('SELECT Service.*, User.username FROM Service JOIN User ON Service.user_id = User.id WHERE Service.id = ?');
 $stmt->execute([$serviceId]);
 $service = $stmt->fetch(PDO::FETCH_ASSOC);
 
