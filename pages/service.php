04d867358659dd7161b3ec61058252a23c36232b
gab1demattos <gab1demattos@yahoo.com>
2025-05-22 00:17:30 +0100

    trying to fix error

--- pages/service.php
+++ pages/service.php
@@ -1,10 +1,8 @@
 <?php
 require_once(__DIR__ . '/../database/session.php');
-require_once(__DIR__ . '/../database/categories.php');
 require_once(__DIR__ . '/../templates/home.tpl.php');
 require_once(__DIR__ . '/../templates/categories.tpl.php');
 require_once(__DIR__ . '/../templates/service.tpl.php');
-
 
 $session = Session::getInstance();
 $user = $session->getUser() ?? null;
