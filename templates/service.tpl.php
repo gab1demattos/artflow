b03e4e127dd9a3eeed1eedc252569784f04dd72e
gab1demattos <gab1demattos@yahoo.com>
2025-05-21 22:48:39 +0100

    changed html code

--- templates/service.tpl.php
+++ templates/service.tpl.php
@@ -7,36 +7,22 @@ $imageRow = $stmtImg->fetch(PDO::FETCH_ASSOC); // Fetch a single row
 $imagePaths = $imageRow['images']; // Extract the 'images' field
 $images = explode(', ', $imagePaths); // Split the string into an array
 
-$stmtOwner = $db->prepare('SELECT u.name FROM User u JOIN Service s ON u.id = s.user_id WHERE s.id = ?');
+$stmtOwner = $db->prepare('SELECT u.name, u.username FROM User u JOIN Service s ON u.id = s.user_id WHERE s.id = ?');
 $stmtOwner->execute([$service['id']]);
 $owner = $stmtOwner->fetch(PDO::FETCH_ASSOC);
 ?>
     <div id="service-display">
-        <div id="service-imgs">
-            <?php foreach ($images as $image) { ?>
-                <img class="thumbnail" src="<?= htmlspecialchars($image) ?>" alt="Service Image">
-            <?php } ?>
-        </div>
-        <div id="service-img">
+        <div id="images-service">
+            <div id="service-imgs">
+                <?php foreach ($images as $image) { ?>
+                    <img class="thumbnail" src="<?= htmlspecialchars($image) ?>" alt="Service Image">
+                <?php } ?>
+            </div>
             <img id="main-image" src="<?= htmlspecialchars($images[0]) ?>" alt="Service Image">
         </div>
-        <div id="service-detail">
-            <div id="service-name">
-                <h2><?= htmlspecialchars($service['title']) ?></h2>
-                <p><?= htmlspecialchars($service['description']) ?></p>
-            </div>
-            <div id="service-info">
-                <p id="price" class="service-info"><?= htmlspecialchars($service['price']) ?>€</p>
-                <div id="service-delivery">
-                    <img src="/images/logos/local_shipping.png" alt="Star Icon">
-                    <p class="service-delivery" class="service-info">Delivery Time: </p>
-                    <p class="service-delivery"><?= htmlspecialchars($service['delivery_time']) ?> days</p>
-                </div>
-                <div id="service-options">
-                    <button id="message" class="service-options">Message <?= htmlspecialchars($owner['name']) ?></button>
-                    <button id="payment" class="service-options">Continue to Payment</button>
-                </div>
-            </div>
+        <div id="service-details">
+            <p><?= htmlspecialchars($service['description']) ?></p>
+            <p><?= htmlspecialchars($owner['name']) . htmlspecialchars($owner['username'])?></p>
             <div id="reviews">
                 <h3>Reviews</h3>
                 <?php
@@ -54,6 +40,22 @@ $owner = $stmtOwner->fetch(PDO::FETCH_ASSOC);
                     <p>No reviews yet. Be the first to leave a review!</p>
                 <?php } ?>
             </div>
+        </div>
+        <div id="service-checkout">
+            <h2><?= htmlspecialchars($service['title']) ?></h2>
+            <div id="service-info">
+                <p id="price" class="service-info"><?= htmlspecialchars($service['price']) ?>€</p>
+                <div id="service-delivery">
+                    <img src="/images/logos/local_shipping.png" alt="Star Icon">
+                    <p class="service-delivery" class="service-info">Delivery Time: </p>
+                    <p class="service-delivery"><?= htmlspecialchars($service['delivery_time']) ?> days</p>
+                </div>
+                <div id="service-options">
+                    <button id="message" class="service-options">Message <?= htmlspecialchars($owner['name']) ?></button>
+                    <button id="payment" class="service-options">Continue to Payment</button>
+                </div>
+            </div>
+        
         </div>
     </div>
     <script src="/js/service-scroll.js"></script>
