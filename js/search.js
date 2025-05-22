340e46d2fa1062e8d72f3c64a4e8f770741e2384
gab1demattos <gab1demattos@yahoo.com>
2025-05-22 17:10:07 +0100

    fixed search results css

--- js/search.js
+++ js/search.js
@@ -2,10 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.getElementById('search-input');
     const searchButton = document.getElementById('search-button');
 
-    searchInput.addEventListener('input', () => {
-        searchButton.style.display = searchInput.value ? 'block' : 'none';
-    });
-
     searchInput.addEventListener('focus', () => {
         searchButton.style.display = 'none';
     });
