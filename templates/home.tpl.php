8e5ca58d80dd845a4dfd33edf675df6cd9e618fb
Maria luiza <up202304306@up.pt>
2025-05-22 14:51:46 +0100

    created a global responsive css

--- templates/home.tpl.php
+++ templates/home.tpl.php
@@ -7,6 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="/css/main.css">
         <link rel="stylesheet" href="/css/see-more.css">
+        <link rel="stylesheet" href="/css/responsive/global-responsive.css">
     </head>
     <body>
     <header>
