dafec688c3377574e053b26e3a7f4456b271cde3
gab1demattos <gab1demattos@yahoo.com>
2025-05-23 13:48:16 +0100

    filter img

--- templates/search.tpl.php
+++ templates/search.tpl.php
@@ -4,9 +4,14 @@
             <button id="search-services" class="active">Services</button>
             <button id="search-names">Usernames</button>
         </div>
-        <div id="search-page-bar">
-                <input type="text" id="search-page-input" placeholder="Search here...">
-                <button id="search-page-button">Search</button>
+        <div id="search-tools">
+            <div>
+                <button id="search-filter"><img src="../images/logos/filter.png"></button>
+            </div>
+            <div id="search-page-bar">
+                    <input type="text" id="search-page-input" placeholder="Search here...">
+                    <button id="search-page-button">Search</button>
+            </div>
         </div>
         <div id="search-results" class="scrollable services-active">
                 
