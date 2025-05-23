84967ed62316f2009dd5d08f1456a221ac37272f
gab1demattos <gab1demattos@yahoo.com>
2025-05-23 15:41:31 +0100

    getting min and max price from db

--- templates/search.tpl.php
+++ templates/search.tpl.php
@@ -23,22 +23,50 @@
                         <label for="filter-option-<?php echo $category['id']; ?>"><?php echo htmlspecialchars($category['category_type']); ?></label>
                     </div>
                 <?php } ?>
+                <h3>Rating</h3>
                 <div id="filter-search-rating">
-                    <h3>Rating</h3>
                     <div class="filter-option-rating">
                         <input type="checkbox" id="filter-option-1" checked>
-                        <label for="filter-option-1">Rating 1</label>
+                        <label for="filter-option-1"> 0</label>
                     </div>
                     <div class="filter-option-rating">
                         <input type="checkbox" id="filter-option-2" checked>
-                        <label for="filter-option-2">Rating 2</label>
+                        <label for="filter-option-2"> 1</label>
                     </div>
                     <div class="filter-option-rating">
-    
                         <input type="checkbox" id="filter-option-3" checked>
-                        <label for="filter-option-3">Rating 3</label>
+                        <label for="filter-option-3"> 2</label>
+                    </div>
+                    <div class="filter-option-rating">
+                        <input type="checkbox" id="filter-option-3" checked>
+                        <label for="filter-option-3"> 3</label>
+                    </div>
+                    <div class="filter-option-rating">
+                        <input type="checkbox" id="filter-option-3" checked>
+                        <label for="filter-option-3"> 4</label>
+                    </div>
+                    <div class="filter-option-rating">
+                        <input type="checkbox" id="filter-option-3" checked>
+                        <label for="filter-option-3"> 5</label>
                     </div>
                 </div>
+                <?php 
+                $stmt = $db->prepare('SELECT MIN(price) as min_price, MAX(price) as max_price FROM Service');
+                $stmt->execute();
+                $priceRange = $stmt->fetch(PDO::FETCH_ASSOC);
+                ?>
+                <h3>Price</h3>
+                <div id="filter-search-price">
+                    <div class="filter-option-price">
+                        <label for="min-price">Min:</label>
+                        <input type="number" id="min-price" placeholder="0" min="0" value="<?php echo htmlspecialchars($priceRange['min_price']); ?>">
+                    </div>
+                    <div class="filter-option-price">
+                        <label for="max-price">Max:</label>
+                        <input type="number" id="max-price" placeholder="1000" min="0" value="<?php echo htmlspecialchars($priceRange['max_price']); ?>">
+                    </div>
+                </div>
+                
             </div>
             <div id="search-results" class="scrollable services-active"></div>
         </div>
