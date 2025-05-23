45aba6eefc9dcc68ebd721ea22a9c3626842c377
gab1demattos <gab1demattos@yahoo.com>
2025-05-23 14:39:04 +0100

    filter

--- templates/search.tpl.php
+++ templates/search.tpl.php
@@ -4,17 +4,28 @@
             <button id="search-services" class="active">Services</button>
             <button id="search-names">Usernames</button>
         </div>
-        <div id="search-tools">
-            <div>
-                <button id="search-filter"><img src="../images/logos/filter.png"></button>
-            </div>
-            <div id="search-page-bar">
-                    <input type="text" id="search-page-input" placeholder="Search here...">
-                    <button id="search-page-button">Search</button>
-            </div>
+        <div id="search-page-bar">
+            <input type="text" id="search-page-input" placeholder="Search here...">
+            <button id="search-page-button">Search</button>
         </div>
-        <div id="search-results" class="scrollable services-active">
-                
+        <div id="search-main">
+            <div id="filter-search">
+                <div id="filter-search-options">
+                    <div class="filter-option">
+                        <input type="checkbox" id="filter-option-1" checked>
+                        <label for="filter-option-1">Option 1</label>
+                    </div>
+                    <div class="filter-option">
+                        <input type="checkbox" id="filter-option-2" checked>
+                        <label for="filter-option-2">Option 2</label>
+                    </div>
+                    <div class="filter-option">
+                        <input type="checkbox" id="filter-option-3" checked>
+                        <label for="filter-option-3">Option 3</label>
+                    </div>
+                </div>
+            </div>
+            <div id="search-results" class="scrollable services-active"></div>
         </div>
     </div>
 <?php } ?>
