e5b6798ac040a72b2c0242d4bbc3f2b7872bd845
gab1demattos <gab1demattos@yahoo.com>
2025-05-23 13:21:49 +0100

    removed close button

--- templates/search.tpl.php
+++ templates/search.tpl.php
@@ -1,6 +1,5 @@
 <?php function drawSearchPage($user) { ?>
     <div class="search-content">
-        <button class="close-modal">x</button>
         <div class="search-options">
             <button id="search-services" class="active">Services</button>
             <button id="search-names">Usernames</button>
