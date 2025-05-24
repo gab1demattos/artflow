ec3a2a6c913bd83b2728c57a86a1f77f4e5a8eca
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-05-24 10:51:08 +0100

    Added a logo

--- templates/home.tpl.php
+++ templates/home.tpl.php
@@ -7,6 +7,7 @@
         <title>artflow</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" href="/images/a.png" type="image/png">
         <link rel="stylesheet" href="/css/main.css">
         <link rel="stylesheet" href="/css/responsive/global-responsive.css">
         <link rel="stylesheet" href="/css/see-more.css">
