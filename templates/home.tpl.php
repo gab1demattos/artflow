a3a9ffa1e6cec6ae0a66ae795986f4018301a608
Maria luiza <up202304306@up.pt>
2025-05-24 13:32:20 +0100

    removed add category button from home page

--- templates/home.tpl.php
+++ templates/home.tpl.php
@@ -110,9 +110,7 @@
                     require_once __DIR__ . '/../database/classes/category.class.php';
                     $session = Session::getInstance();
                     $user = $session->getUser();
-                    if ($user && isset($user['user_type']) && $user['user_type'] === 'admin'): ?>
-                        <button id="open-category-modal" class="button filled hovering" type="button" style="margin-bottom:2em;">Add Category</button>
-                    <?php endif; ?>
+                    ?>
                     <div id="category-list">
                         <?php
                         $categories = Category::getCategories();
