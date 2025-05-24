7a2b9128e12aec9b45dec05b6b67e6ea213c1317
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-05-24 12:37:41 +0100

    Quick fix

--- actions/login/signup-action.php
+++ actions/login/signup-action.php
@@ -1,7 +1,7 @@
 <?php
 
 declare(strict_types=1);
-require_once(__DIR__ . '/../../database/security_bootstrap.php');
+require_once(__DIR__ . '/../../database/security/security_bootstrap.php');
 require_once(__DIR__ . '/../../database/session.php');
 require_once(__DIR__ . '/../../database/classes/user.class.php');
 require_once(__DIR__ . '/../../database/database.php');
